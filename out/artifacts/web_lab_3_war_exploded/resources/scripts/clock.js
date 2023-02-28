c0d9eb074796089078a5258fa42b7cd8293dbbde
DanilDuck <101825692+DanilDuck@users.noreply.github.com>
2023-02-28 15:44:55 +0300

    Delete out/artifacts/web_lab_3_war_exploded directory

--- out/artifacts/web_lab_3_war_exploded/resources/scripts/clock.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    $('#clock').html(moment().format('D.MM.YYYY HH:mm:ss'));
-}
-setInterval(() => {
-    $('#clock').html(moment().format('D.MM.YYYY HH:mm:ss'))
-}, 5000);
